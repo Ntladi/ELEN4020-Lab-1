bda0ec15697b7883c632c7a5e99dc436edc91695
Ntladi Mohajane <53006060+Ntladi@users.noreply.github.com>
2020-02-16 18:06:30 +0200

    Cleaned up 2D OpenMP Implementation

--- matrix_2d_parallel_openmp.c
+++ matrix_2d_parallel_openmp.c
@@ -1,48 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
-#define NUM_THREADS 1
+
+#define NUM_THREADS 4
+#define MAT_LENGTH 3
+#define MAT_SQUARE MAT_LENGTH*MAT_LENGTH
+
+
+int matrix_A[MAT_SQUARE] = {9,15,37,1,93,7,108,117,203};
+int matrix_B[MAT_SQUARE] = {115,197,6,7,5,11,113,19,17};
+int matrix_C[MAT_SQUARE];
+
+void rank2TensorMultOpenMP();
+int indexMultiplication(int index);
 
 void main(void)
 {
-	int matrix_A[9] = {9,15,37,1,93,7,108,117,203};
-	int matrix_B[9] = {115,197,6,7,5,11,113,19,17};
-	int matrix_C[9];
+	rank2TensorMultOpenMP();	
+}
 
+void rank2TensorMultOpenMP()
+{
 	omp_set_num_threads(NUM_THREADS);
 	#pragma omp parallel
 	{
 		int thread_id = omp_get_thread_num(); 
 		int threads_given = omp_get_num_threads();
 
-		for(int index=thread_id; index<9; index+=threads_given)
-		{
-			int sum = 0;
-
-			for(int row=0, col=0; row<3; row++, col+=3)
-			{
-				int row_offset = ((index/3)*3);
-				int col_offset = (index%3);
-				sum += matrix_A[row + row_offset] * matrix_B[col + col_offset];		
-			}
-
-			matrix_C[index] = sum;
-
-			//printf("sum = %d, ", sum);
-			//printf("thread id = %d, ", thread_id);
-			//printf("index id = %d \n", t);
-		}
-	
+		for(int index = thread_id; index < MAT_SQUARE; index += threads_given)
+			matrix_C[index] = indexMultiplication(index);
 	}
 
-	for(int i=0; i<9; i++)
+	for(int index = 0; index < MAT_SQUARE; index ++)
 	{
-		printf("%d ",matrix_C[i]);
-		if(i%3==2) printf("\n");
+		printf("%d ",matrix_C[index]);
+		if(index % MAT_LENGTH == MAT_LENGTH - 1) printf("\n");
+	}
+}
+
+int indexMultiplication(int index)
+{
+	int sum = 0;
+
+	for(int row = 0, col = 0; row < MAT_LENGTH; row ++, col += MAT_LENGTH)
+	{
+		int row_offset = ((index / MAT_LENGTH) * MAT_LENGTH);
+		int col_offset = (index % MAT_LENGTH);
+		sum += matrix_A[row + row_offset] * matrix_B[col + col_offset];		
 	}
 
-	
-
-
-	
+	return sum;
 }
