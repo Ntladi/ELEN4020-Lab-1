b4283be4e2149d5cf6f7ec5e51c42746a9e1228f
Zwavhu <52232461+Zwavhu@users.noreply.github.com>
2020-02-23 03:22:51 +0200

    Added 64 thread benchmark for 2D PThreads

--- scripts/pthreads_2d.sh
+++ scripts/pthreads_2d.sh
@@ -20,6 +20,9 @@ echo "Computing a 10x10 matrix using Pthreads with 4 threads:"
 echo "Computing a 10x10 matrix using Pthreads with 8 threads:"
 { time ../executables/pthreads_2d 8 10 ls -l >/dev/null; } 2>&1 | grep real
 
+echo "Computing a 10x10 matrix using Pthreads with 64 threads:"
+{ time ../executables/pthreads_2d 64 10 ls -l >/dev/null; } 2>&1 | grep real
+
 # 20 x 20 matrix
 
 echo ""
@@ -35,6 +38,9 @@ echo "Computing a 20x20 matrix using Pthreads with 4 threads:"
 
 echo "Computing a 20x20 matrix using Pthreads with 8 threads:"
 { time ../executables/pthreads_2d 8 20 ls -l >/dev/null; } 2>&1 | grep real
+
+echo "Computing a 20x20 matrix using Pthreads with 64 threads:"
+{ time ../executables/pthreads_2d 64 20 ls -l >/dev/null; } 2>&1 | grep real
 
 # 30 x 30 threads
 
@@ -52,6 +58,9 @@ echo "Computing a 30x30 matrix using Pthreads with 4 threads:"
 echo "Computing a 30x30 matrix using Pthreads with 8 threads:"
 { time ../executables/pthreads_2d 8 30 ls -l >/dev/null; } 2>&1 | grep real
 
+echo "Computing a 30x30 matrix using Pthreads with 64 threads:"
+{ time ../executables/pthreads_2d 64 30 ls -l >/dev/null; } 2>&1 | grep real
+
 # 100 x 100 matrix
 
 echo ""
@@ -68,6 +77,9 @@ echo "Computing a 100x100 matrix using Pthreads with 4 thread:"
 echo "Computing a 100x100 matrix using Pthreads with 8 threads:"
 { time ../executables/pthreads_2d 8 100 ls -l >/dev/null; } 2>&1 | grep real
 
+echo "Computing a 100x100 matrix using Pthreads with 64 threads:"
+{ time ../executables/pthreads_2d 64 100 ls -l >/dev/null; } 2>&1 | grep real
+
 # 1000 x 1000 matrix
 
 echo ""
@@ -83,3 +95,6 @@ echo "Computing a 1000x1000 matrix using Pthreads with 4 threads:"
 
 echo "Computing a 1000x1000 matrix using Pthreads with 8 threads:"
 { time ../executables/pthreads_2d 8 1000 ls -l >/dev/null; } 2>&1 | grep real
+
+echo "Computing a 1000x1000 matrix using Pthreads with 64 threads:"
+{ time ../executables/pthreads_2d 64 1000 ls -l >/dev/null; } 2>&1 | grep real
